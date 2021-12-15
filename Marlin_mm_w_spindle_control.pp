3ec695628ec598fd40cfb7f49606c464aba6fe20
Edward Webber <github@webbsense.com>
2021-12-14 21:53:12 -0600

    +Added helical-arc support =Changed to unix line endings =Improved comments =Increased plunge speed when above material =Now uses machine default rapid move speed =Disabled PLUNGE_RATE section to avoid slowdowns =Comments now report carved Z depth, not material Z

--- Marlin_mm_w_spindle_control.pp
+++ Marlin_mm_w_spindle_control.pp
@@ -1,164 +1,203 @@
 +===========================================================================
-+
-+ gCode - Vectric machine output post-processor for vCarve and Aspire
-+
+|
+| gCode - Vectric machine output post-processor for vCarve and Aspire
+|
 +===========================================================================
-+
-+ History
-+
-+ Who       When       What
-+ ========  ========== =====================================================
-+ EdwardW   01/13/2020 Initial authoring
-+                      Added status messages (M117)
-+                      Enabled Arc movements (G2/G3)
-+                      Added ending presentation
-+ EdwardW   02/28/2020
-+                      Added G54 (CNC) coordinate support
-+ EdwardW   10/26/2021
-+                      Added router control (M3/M5)
-+                      Switch to G53 (Machine Coords) during end script
-+                      Removed tool change support because its unworkable
+|
+| History
+|
+| Who       When       What
+| ========  ========== =====================================================
+| EdwardW   01/13/2020 Initial authoring
+|                      Added status messages (M117)
+|                      Enabled Arc movements (G2/G3)
+|                      Added ending presentation
+| EdwardW   02/28/2020
+|                      Added G54 (CNC) coordinate support
+| EdwardW   10/26/2021
+|                      Added router control (M3/M5)
+|                      Removed tool change support because its unworkable
+| EdwardW   12/14/2021
+|                      Added helical-arc support
+|                      Changed to unix line endings
+|                      Improved comments
+|                      Increased plunge speed when above material
+|                      Now uses machine default rapid move speed
+|                      Disabled PLUNGE_RATE section to avoid slowdowns
+|                      Comments now report carved Z depth, not material Z
 +===========================================================================
 
 POST_NAME = "Marlin M0 G54 Arc (mm) (*.gcode)"
 
 FILE_EXTENSION = "gcode"
 
-UNITS = "MM"
+UNITS = "mm"
 
 +---------------------------------------------------------------------------
-+    Configurable items based on your CNC
+|    Configurable items based on your CNC
 +---------------------------------------------------------------------------
-+ Use 1-100 for spindle speeds instead of true speeds of 10000-27500
++ Use 1-100 (%) for spindle speeds instead of true speeds of 10000-27500 (rpm)
 SPINDLE_SPEED_RANGE = 1 100 10000 27500
 
-+ Replace all () with {} to avoid gCode interpretation errors
-SUBSTITUTE = "(<)>"
++ Replace all <> with () to avoid gCode interpretation errors
+SUBSTITUTE = "<(>)"
+
++ Plunge moves to Plunge (Z2) height are rapid moves
+RAPID_PLUNGE_TO_STARTZ = "YES"
 
 +---------------------------------------------------------------------------
-+    Line terminating characters
+|    Line terminating characters
 +---------------------------------------------------------------------------
 + Use windows-based line endings \r\n
-LINE_ENDING = "[13][10]"
++ LINE_ENDING = "[13][10]"
+
++ Use unix-based line endings \n
+LINE_ENDING = "[10]"
 
 +---------------------------------------------------------------------------
-+    Block numbering
+|    Block numbering
 +---------------------------------------------------------------------------
 LINE_NUMBER_START     = 0
 LINE_NUMBER_INCREMENT = 1
 LINE_NUMBER_MAXIMUM = 999999
 
 +===========================================================================
-+
-+    Formatting for variables
-+
+|
+|    Formatting for variables
+|
 +===========================================================================
 
 VAR LINE_NUMBER = [N|A|N|1.0]
 VAR SPINDLE_SPEED = [S|A|S|1.0]
-VAR FEED_RATE = [F|C|F|1.1]
-VAR CUT_RATE = [FC|C|F|1.1]
-VAR PLUNGE_RATE = [FP|C|F|1.1]
-VAR X_POSITION = [X|C|X|1.3]
-VAR Y_POSITION = [Y|C|Y|1.3]
-VAR Z_POSITION = [Z|C|Z|1.3]
-VAR ARC_CENTRE_I_INC_POSITION = [I|A|I|1.3]
-VAR ARC_CENTRE_J_INC_POSITION = [J|A|J|1.3]
-VAR X_HOME_POSITION = [XH|A|X|1.3]
-VAR Y_HOME_POSITION = [YH|A|Y|1.3]
-VAR Z_HOME_POSITION = [ZH|A|Z|1.3]
-VAR X_LENGTH = [XLENGTH|A|W:|1.0]
-VAR Y_LENGTH = [YLENGTH|A|H:|1.0]
-VAR Z_LENGTH = [ZLENGTH|A|Z:|1.1]
+VAR FEED_RATE = [F|A|F|1.0]
+VAR CUT_RATE = [FC|A|F|1.0]
+VAR PLUNGE_RATE = [FP|A|F|1.0]
+VAR X_POSITION = [X|C| X|1.3]
+VAR Y_POSITION = [Y|C| Y|1.3]
+VAR Z_POSITION = [Z|C| Z|1.3]
+VAR ARC_CENTRE_I_INC_POSITION = [I|A| I|1.3]
+VAR ARC_CENTRE_J_INC_POSITION = [J|A| J|1.3]
+VAR X_HOME_POSITION = [XH|A| X|1.3]
+VAR Y_HOME_POSITION = [YH|A| Y|1.3]
+VAR Z_HOME_POSITION = [ZH|A| Z|1.3]
++ VAR X_LENGTH = [XLENGTH|A|W:|1.0]
++ VAR Y_LENGTH = [YLENGTH|A|H:|1.0]
++ VAR Z_LENGTH = [ZLENGTH|A|Z:|1.0]
+VAR X_LENGTH = [XLENGTH|A||1.0]
+VAR Y_LENGTH = [YLENGTH|A||1.0]
+VAR Z_LENGTH = [ZLENGTH|A||1.0]
+VAR Z_MIN = [ZMIN|A||1.0]
+VAR SAFE_Z_HEIGHT = [SAFEZ|A||1.3]
+VAR DWELL_TIME = [DWELL|A|S|1.2]
+
 
 +===========================================================================
-+
-+    Block definitions for toolpath output
-+
+|
+|    Block definitions for toolpath output
+|
 +===========================================================================
 
 +---------------------------------------------------------------------------
-+  Commands output at the start of the file
+|  Start of file output
 +---------------------------------------------------------------------------
-
 begin HEADER
 
 "; [TP_FILENAME]"
-"; Generated [DATE] [TIME]"
-"; Material size: [XLENGTH] [YLENGTH] [ZLENGTH] [UNITS]"
-"; Safe Z height: [SAFEZ] mm"
+"; Material size: [YLENGTH]x[XLENGTH]x[ZMIN]mm"
 "; Tools: [TOOLS_USED]"
-"; Notes: [FILE_NOTES]"
+"; Paths: [TOOLPATHS_OUTPUT]"
+"; Safe Z: [SAFEZ]mm"
+"; Generated on [DATE] [TIME] by [PRODUCT]"
 "G90"
-"G21"
-"M117 [XLENGTH] [YLENGTH] [ZLENGTH] [UNITS] w/#[T] [TOOLNAME]"
-"M0 Continue when ready"
+"G20"
+"M117 [YLENGTH]x[XLENGTH]x[ZMIN]mm  Bit #[T]"
+"M0 Load [TOOLNAME]"
 "G54"
-"G0 Z[SAFEZ] [F]"
-"G0 [XH] [YH] [F]"
+"G0 Z[SAFEZ]"
+"G0 [XH][YH]"
++ Manually set spindle to 100% since I have no VFD. Otherwise use: M3 [S]
 "M3 S100"
-"; Tool [T]: [TOOLNAME]"
 "; Path: [TOOLPATH_NAME]"
-"; [TOOLPATH_NOTES]"
+"; Tool [T]: [TOOLNAME] ([TOOL_NOTES])"
 
 +---------------------------------------------------------------------------
-+  Commands output for rapid moves
+|  Rapid (no load) move
 +---------------------------------------------------------------------------
-
 begin RAPID_MOVE
 
-"G0 [X] [Y] [Z] [F]"
+"G0 [X][Y][Z]"
 
 +---------------------------------------------------------------------------
-+  Commands output for feed rate moves
+|  Carving move
 +---------------------------------------------------------------------------
-
 begin FEED_MOVE
 
-"G1 [X] [Y] [Z] [FC]"
+"G1 [X][Y][Z] [FC]"
 
 +---------------------------------------------------------------------------
-+ Commands output for Plunge Moves
+|  Plunging move - Only enable if necessary. Can cause huge slowdowns
 +---------------------------------------------------------------------------
-begin PLUNGE_MOVE
++begin PLUNGE_MOVE
 
-"G1 [X] [Y] [Z] [FP]"
++"G1 [X][Y][Z] [FP]"
 
 +---------------------------------------------------------------------------
-+  Commands output for clockwise arc  move
+|  Clockwise arc move
 +---------------------------------------------------------------------------
-
 begin CW_ARC_MOVE
 
-"G2 [X] [Y] [I] [J] [FC]"
+"G2 [X][Y][I][J] [FC]"
 
 +---------------------------------------------------------------------------
-+  Commands output for counterclockwise arc  move
+|  Counterclockwise arc move
 +---------------------------------------------------------------------------
-
 begin CCW_ARC_MOVE
 
-"G3 [X] [Y] [I] [J] [FC]"
+"G3 [X][Y][I][J] [FC]"
+
++---------------------------------------------------
++  Clockwise helical-arc move
++---------------------------------------------------
+begin CW_HELICAL_ARC_MOVE
+
+"G2 [X][Y][Z][I][J] [FC]"
+
++---------------------------------------------------
++  Counterclockwise helical-arc move
++---------------------------------------------------
+begin CCW_HELICAL_ARC_MOVE
+
+"G3 [X][Y][Z][I][J] [FC]"
 
 +---------------------------------------------------------------------------
-+  Commands output for toolpath changes
+|  Begin new toolpath
 +---------------------------------------------------------------------------
-
 begin NEW_SEGMENT
 
-"; Path: [TOOLPATH_NAME] [PATHNAME]"
-"; [TOOLPATH_NOTES]"
-"M117 [TOOLPATH_NAME] @ [S]%"
+";==========================================================================="
+";"
+";                       Path: [TOOLPATH_NAME]"
+";"
+";==========================================================================="
+"M117 [TOOLPATH_NAME] - Bit #[T]"
+
++ Overriding to 100% since I have no VFD. Otherwise use M3 [S]
 "M3 S100"
 
-+---------------------------------------------------------------------------
-+  Commands output at the end of the file
-+---------------------------------------------------------------------------
++---------------------------------------------
++  Dwell (momentary pause)
++---------------------------------------------
+begin DWELL_MOVE
 
+"G4 [DWELL]"
+
++---------------------------------------------------------------------------
+|  End of file output
++---------------------------------------------------------------------------
 begin FOOTER
 
 "M5"
 "M117 Returning home"
-"G0 [ZH] [F]"
-"G0 [XH] [YH] [F]"
+"G0 [ZH]"
+"G0 [XH][YH]"
 "M117 Routing complete."
